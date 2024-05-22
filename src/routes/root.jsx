cf92c41f0e4864fed51387f38b84cb23dd0958f7
solitdio079 <solitdio079@gmail.com>
2024-05-22 22:53:05 +0300

    responsive navbarfinale

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -124,7 +124,7 @@ export default function Root() {
               <img width="120" src={logo}></img>
             </NavLink>
           </div>
-          <div className="hidden lg:flex-1">
+          <div className="hidden lg:flex">
             <NavLink
               to="/"
               className={({ isActive, isPending }) =>
