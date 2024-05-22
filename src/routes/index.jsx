23dc24adde346fc595f6c744f0289168bd3ce39e
solitdio079 <solitdio079@gmail.com>
2024-05-22 14:49:41 +0300

    hero changed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,48 +4,23 @@ import hero2 from '../assets/hero2.jpeg'
 export default function Index() {
     return (
       <>
-        <div className="carousel carousel-end">
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-              alt="Drink"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-              alt="Drink"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-              alt="Drink"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-              alt="Drink"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-              alt="Drink"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-              alt="Drink"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-              alt="Drink"
-            />
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage:
+              `url(${hero2})`,
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">FasoCard</h1>
+              <p className="mb-5">
+                Écologique et Durable : Réduisez l&apos;utilisation de papier
+                avec cette solution écologique et robuste.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
           </div>
         </div>
         <div className="hero  bg-base-200">
