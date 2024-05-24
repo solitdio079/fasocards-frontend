a19cf7f5f782069360c5207b382564559cc928ed
solitdio079 <solitdio079@gmail.com>
2024-05-25 02:37:16 +0300

    added real data to owner list

---
+++ src/components/list-card.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import { FaX, FaEye, FaPencil } from 'react-icons/fa6'
+import hero1 from '../assets/hero1.jpeg'
+import { Link } from 'react-router-dom'
+export default function ListCard({ data }) {
+  return (
+    <>
+      <div className="card w-full lg:w-1/3 bg-base-100 shadow-xl image-full">
+        <figure>
+          <img src={hero1} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {data.name}{' '}
+            <div className="badge badge-secondary"> {data.email} </div>
+          </h2>
+          <p> {data.description} </p>
+          <div className="card-actions justify-end">
+          
+              <Link to={`/business/${data.name}`} className="btn btn-primary">
+                <FaEye /> Voir
+              </Link>
+           
+            <Link to={`/edit/${data.name}`} className="btn btn-warning">
+              <FaPencil /> Modifier
+            </Link>
+            <Link to={`/delete/${data._id}`} className="btn btn-error">
+              <FaX /> Supprimer
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/routes/owner-list.jsx
+++ src/routes/owner-list.jsx
@@ -1,6 +1,6 @@
-import {FaX, FaEye, FaPencil } from 'react-icons/fa6'
-import hero1 from '../assets/hero1.jpeg'
+import { useNavigation, useLoaderData,Link} from 'react-router-dom'
 
+import ListCard from '../components/list-card'
 export async function loader({ params }) {
     console.log(params.email)
     const response = await fetch(
@@ -23,35 +23,23 @@ export async function loader({ params }) {
 }
 
 export default function OwnerList() {
+    const navigation = useNavigation()
+    const data = useLoaderData()
     return (
       <>
-        <div className="flex m-5 items-center justify-between flex-wrap">
-          <div className="card w-full lg:w-1/3 bg-base-100 shadow-xl image-full">
-            <figure>
-              <img
-                src={hero1}
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                Shoes! <div className="badge badge-secondary">abc@abv.com</div>
-              </h2>
-              <p>The description is a mess without the 20 characters rule!</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">
-                  <FaEye /> Voir
-                </button>
-                <button className="btn btn-warning">
-                  <FaPencil /> Modifier
-                </button>
-                <button className="btn btn-error">
-                  <FaX /> Supprimer
-                </button>
-              </div>
-            </div>
+        {navigation.state === 'idle' ? (
+          <div className="flex m-5 items-center justify-between flex-wrap">
+            {data.length === 0 ? (
+              <Link className="btn btn-primary" to="/create/business">
+                Ajouter un business
+              </Link>
+            ) : (
+              data.map((item) => <ListCard key={item._id} data={item} />)
+            )}
           </div>
-        </div>
+        ) : (
+          <span className="loading loading-infinity loading-lg"></span>
+        )}
       </>
     )
 }
