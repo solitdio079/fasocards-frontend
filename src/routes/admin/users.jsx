f05c08cc2dbd38695bb5e97c4794b90ea13fb328
solitdio079 <solitdio079@gmail.com>
2024-06-15 23:32:14 +0300

    avatar removed

--- src/routes/admin/users.jsx
+++ src/routes/admin/users.jsx
@@ -31,13 +31,7 @@ export default function Users() {
     return (
       <div className="flex flex-col lg:flex-row">
         <div className="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <div className="avatar">
-              <div className="w-24 rounded-full">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-            </div>
-          </figure>
+        
           <div className="card-body">
             <h2 className="card-title">Shoes!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
