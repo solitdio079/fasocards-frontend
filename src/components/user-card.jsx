2c1841ff454d7f0f221a082c8a73c36c8da1f2b3
solitdio079 <solitdio079@gmail.com>
2024-06-16 01:47:08 +0300

    userFetcher

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -1,20 +1,23 @@
 /* eslint-disable react/prop-types */
-import {Form} from 'react-router-dom'
+import {Form, useFetcher} from 'react-router-dom'
 
-export default function UserCard({user}) {
+export default function UserCard({ user }) {
+    const fetcher = useFetcher()
     return (
       <div className="card w-96 bg-base-100 shadow-xl m-2">
         <div className="card-body">
           <h2 className="card-title"> {user.email} </h2>
           <p className="badge badge-primary"> {user.admin ? 'Admin': 'Client'} </p>
           <div className="card-actions justify-end">
-            <Form method="post">
-              <input
+                    <fetcher.Form method="post">
+                        <input type="hidden" name="id" value={user._id} /> 
+                        <input
+                            name="isAllowed"
                 type="checkbox"
                 defaultChecked = {user.isAllowed}
                 className="checkbox checkbox-md checkbox-info"
               />
-            </Form>
+            </fetcher.Form>
           </div>
         </div>
       </div>
