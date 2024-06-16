c76c1f032dc525cedc6cd78b55fdbe9a15f93a1d
solitdio079 <solitdio079@gmail.com>
2024-06-16 13:06:59 +0300

    submitting everything

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -7,18 +7,23 @@ export default function UserCard({ user }) {
       <div className="card w-96 bg-base-100 shadow-xl m-2">
         <div className="card-body">
           <h2 className="card-title"> {user.email} </h2>
-          <p className="badge badge-primary"> {user.admin ? 'Admin': 'Client'} </p>
+          <p className="badge badge-primary">
+            {' '}
+            {user.admin ? 'Admin' : 'Client'}{' '}
+          </p>
           <div className="card-actions justify-end">
-                    <fetcher.Form method="post">
-                        <input type="hidden" name="id" value={user._id} /> 
-                        <button type="submit">
-                        <input
-                            name="isAllowed"
-                            type="checkbox"
-                          
-                defaultChecked = {user.isAllowed}
-                className="checkbox checkbox-md checkbox-info"
-              /></button>
+            <fetcher.Form method="post">
+              <input type="hidden" name="id" value={user._id} />
+              <input type="hidden" name="email" value={user.email} />
+             
+                <input
+                  name="isAllowed"
+                  type="checkbox"
+                  defaultChecked={user.isAllowed}
+                  className="checkbox checkbox-md checkbox-info"
+                        />
+                        <button type="submit" className='btn btn-primary'>Switch</button>
+              
             </fetcher.Form>
           </div>
         </div>
