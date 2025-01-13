3ef05aecfef2eeed488c9170cabd1ef222b1d053
solitdio079 <solitdio079@gmail.com>
2025-01-13 10:07:50 +0300

    bg opacity done

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -14,7 +14,7 @@ export default function Index() {
           backgroundImage: `url(${hero3})`,
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay bg-opacity-40"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">FasoCard</h1>
